527d1b28136b4911e12cb0d3ef193eddae55f957
周旭鹏 <731299128@qq.com>
2018-09-12 13:13:08 +0800

    add CSharpHomework.

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String c = " ";
+            int num_1, num_2, product;
+            System.Console.Write("两个数做乘法，请输入第一个数字: ");
+            c = Console.ReadLine();
+            num_1 = Int32.Parse(c);
+            System.Console.Write("请输入第二个数字: ");
+            c = Console.ReadLine();
+            num_2 = Int32.Parse(c);
+            product = num_1 * num_2;
+            Console.WriteLine("乘积为: " + product);
+        }
+    }
+}
---
+++ homework2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        double num_1, num_2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s_1 = textBox1.Text;
+            num_1 = Double.Parse(s_1);
+            string s_2 = textBox2.Text;
+            num_2 = Double.Parse(s_2);
+
+            double product;
+            product = num_1 * num_2;
+            //显示乘积结果
+            this.richTextBox1.Text = "" + product;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
