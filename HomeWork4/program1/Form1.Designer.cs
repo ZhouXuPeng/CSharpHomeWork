6e7fffa93663140432c16f3d93f586ffe9d18148
周旭鹏 <731299128@qq.com>
2018-10-08 15:56:42 +0800

    forth homework

---
+++ HomeWork4/program1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace program1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(433, 152);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "设置时间：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(550, 149);
+            this.maskedTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.maskedTextBox1.Mask = "90时90分";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(118, 30);
+            this.maskedTextBox1.TabIndex = 1;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(499, 262);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开启闹铃！";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "关闭闹铃！";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1634, 719);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HomeWork4/program2/Program.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    //订单
+    interface Order
+    {
+        void AddOrder(int OrderNumber, string TradeName, string Customer);//添加订单
+        void RemoveOrder(int element);//删除订单
+        void ChangeOrder(int originated, int changed, int flag);//修改订单 （订单号 flag 为0）
+        void ChangeOrder(string originated, string changed, int flag);//修改订单 （商品名称 flag 为1、客户 flag 为2）
+        void InquriyOrder(int ordernumber, int flag);//查找并打印订单 （订单号 flag 为0）
+        void InquriyOrder(string ordername, int flag);//查找并打印订单 （商品名称 flag 为1、客户 flag 为2）
+        void OrderPrint();//打印全部订单
+    }
+
+    //订单服务
+    public class OrderService : Order
+    {
+        //设置变量
+        private int OrderNumber;
+        private string TradeName;
+        private string Customer;
+
+        //储存订单订单
+        List<int> OrderNumbers = new List<int> { }; //创建订单号数组
+        List<string> TradeNames = new List<string> { }; //创建商品名数组
+        List<string> Customers = new List<string> { }; //创建顾客数组
+
+        //添加订单
+        public void AddOrder(int OrderNumber, string TradeName, string Customer)
+        {
+            OrderNumbers.Add(OrderNumber);
+            TradeNames.Add(TradeName);
+            Customers.Add(Customer);
+        }
+
+        //删除订单
+        public void RemoveOrder(int element)
+        {
+            element = element - 1;//向前推一个
+
+            int sum = 0;
+            foreach (int a in OrderNumbers)//获取数组长度
+            {
+                sum++;
+            }
+
+            int i = 0;
+            for(int n = 0; n < sum; n++)
+            {
+                if(i == element)
+                {
+                    int b = i;
+                    for (int a = 0; a < sum - 1 - element; a++)
+                    {
+                        OrderNumbers[b] = OrderNumbers[b + 1];
+                        TradeNames[b] = TradeNames[b + 1];
+                        Customers[b] = Customers[b + 1];
+                        b++;
+                    }
+                    OrderNumbers[b] = 0;
+                    TradeNames[b] = null;
+                    Customers[b] = null;
+                    break;
+                }
+                i++;
+            }
+
+        }
+
+
+        //查询订单(按照订单号、商品名称、客户字段进行查询)
+        public void InquriyOrder(int ordernumber, int flag)
+        {
+            //订单号查询 flag为 0
+            if (flag == 0)
+            {
+                int i = 0;
+                foreach(int n in OrderNumbers)
+                {
+                    if(n == ordernumber)
+                    {
+                        Console.Write("\n按照订单号查询结果为:\n" + "订单号:" + OrderNumbers[i] + "\n商品名称:" +
+                            TradeNames[i] + "\n客户:" + Customers[i] + "\n");
+                        break;
+                    }
+                    i++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("您输入的信息有误!");
+            }
+        }
+        public void InquriyOrder(string ordername, int flag)
+        {
+            //商品名称查询 flag为 1
+            if (flag == 1)
+            {
+                int i = 0;
+                foreach (string n in TradeNames)
+                {
+                    if (n == ordername)
+                    {
+                        Console.Write("\n按照商品名称查询结果为:\n" + "订单号:" + OrderNumbers[i] + "\n商品名称:" +
+                            TradeNames[i] + "\n客户:" + Customers[i] + "\n");
+
+                        break;
+                    }
+                    i++;
+                }
+            }
+            //客户查询 flag为 2
+            else if (flag == 2)
+            {
+                int i = 0;
+                foreach (string n in Customers)
+                {
+                    if (n == ordername)
+                    {
+                        Console.Write("\n按照客户查询结果为:\n" + "订单号:" + OrderNumbers[i] + "\n商品名称:" +
+                            TradeNames[i] + "\n客户:" + Customers[i] + "\n");
+
+                        break;
+                    }
+                    i++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("您输入的信息有误!");
+            }
+        }
+
+        //修改订单
+        public void ChangeOrder(int originated, int changed, int flag)
+        {
+            //订单号修改 flag为 0
+            if (flag == 0)
+            {
+                int sum = 0;
+                foreach (int a in OrderNumbers)//获取数组长度
+                {
+                    sum++;
+                }
+
+                int i = 0;
+                foreach (int n in OrderNumbers)
+                {
+                    if (n == originated)
+                    {
+                        OrderNumbers[i] = changed; //找到后进行修改
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("您输入的信息有误!");
+            }
+        }
+        public void ChangeOrder(string originated, string changed, int flag)
+        {
+            int sum = 0;
+            foreach (int a in OrderNumbers)//获取数组长度
+            {
+                sum++;
+            }
+
+            //商品名称修改 flag为 1
+            if (flag == 1)
+            {
+                int i = 0;
+                foreach (string n in TradeNames)
+                {
+                    if (n == originated)
+                    {
+                        TradeNames[i] = changed; //找到后进行修改
+                        break;
+                    }
+                }
+            }
+            //客户修改 flag为 2
+            else if (flag == 2)
+            {
+                int i = 0;
+                foreach (string n in Customers)
+                {
+                    if (n == originated)
+                    {
+                        Customers[i] = changed; //找到后进行修改
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("您输入的信息有误!");
+            }
+        }
+
+        //打印订单
+        public void OrderPrint()
+        {
+            Console.WriteLine("打印全部订单:");
+            foreach (string a in Customers)
+            {
+                Console.Write(a + "\t");
+            }
+            Console.Write("\n");
+            foreach (string b in TradeNames)
+            {
+                Console.Write(b + "\t");
+            }
+            Console.Write("\n");
+            foreach (int c in OrderNumbers)
+            {
+                Console.Write(c + "\t");
+            }
+            Console.WriteLine("");
+        }
+
+    }
+
+    //订单明细
+    class OrderDetails : OrderService
+    {
+        //重载ToString()
+        //public override string ToString()
+        //{
+        //    foreach(string a in Customers)
+        //    return "订单信息";
+        //}
+
+
+        //public new void OrderPrint(OrderService order){ }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //添加订单
+            Order order = new OrderService();
+            order.AddOrder(111, "汽车", "周旭鹏");
+            order.AddOrder(112, "电动车", "张三");
+            order.AddOrder(113, "自行车", "李四");
+
+            order.OrderPrint();//订单全部打印
+
+            order.InquriyOrder(112, 0);//按照订单号查询(参数为 0 )，并输出
+            order.InquriyOrder("自行车", 1);//按照商品名称查询(参数为 1 )，并输出
+            order.InquriyOrder("周旭鹏", 2);//按照客户查询(参数为 2 )，并输出
+
+            order.RemoveOrder(2);//删除第二个订单
+
+            order.ChangeOrder("周旭鹏", "王五", 2);//修改订单（将 “周旭鹏”改为 “王五”）
+
+            order.OrderPrint();//打印修改、删除后的全部订单
+
+            // try,catch,finish 的异常处理 ！！！！！！！！！！因为使用了参数（0-订单号，1-商品名称，2-客户）和if-else语句,所以运行以下错误代码不会报错，所以未使用 try-catch 语句
+            //order.InquriyOrder(114, 0); //查找错误
+            //order.ChangeOrder("周", "王五", 2); //传参错误
+
+
+            //订单明细(未能正常运行)
+            //OrderDetails orderdetails = new OrderDetails();
+            //orderdetails.OrderPrint(order);
+        }
+    }
+}
