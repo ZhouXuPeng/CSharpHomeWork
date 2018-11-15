c248632fccf427235e8eb47d051dccb2d4db350d
周旭鹏 <731299128@qq.com>
2018-11-15 22:25:44 +0800

    eighth homework

---
+++ Homework8/ordertest/Common.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordertest
+{
+    public class Common // static 不是必须
+    {
+        private int Flag { set; get; }
+        public Common()
+        {
+            Flag = 0;
+        }
+    }
+}
